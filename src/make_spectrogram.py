32b3a007f17074587bcb3ee38926c851ff606f78
Ryan Coffee <coffeer76@gmail.com>
2022-07-07 14:25:29 -0700

    updated for Ryan hive test

--- src/make_spectrogram.py
+++ src/make_spectrogram.py
@@ -8,15 +8,22 @@ import sys
 def main():
     fname = sys.argv[1]
     samplerate,data = wavfile.read(fname)
-    nsamples = 512
-    nfolds = data.shape[0]//nsamples
+    nsamples = 1<<12 
+    nfolds = data.shape[0]>>10
+    nfolds = 1<<12
     sz = nfolds*nsamples
-    ldata = data[:sz,0].reshape((nfolds,-1))
-    rdata = data[:sz,1].reshape((nfolds,-1))
-    RDATA = dct(np.column_stack((rdata,np.flip(rdata,axis=1))),type=3,axis=1)
-    LDATA = dct(np.column_stack((ldata,np.flip(ldata,axis=1))),type=3,axis=1)
-    np.savetxt('%s.rspect'%fname,np.abs(RDATA[:,:nsamples*2:2]).T)
-    np.savetxt('%s.lspect'%fname,np.abs(LDATA[:,:nsamples*2:2]).T)
+    if len(data.shape)>1:
+        ldata = data[:sz,0].reshape((nfolds,-1))
+        rdata = data[:sz,1].reshape((nfolds,-1))
+        RDATA = dct(np.column_stack((rdata,np.flip(rdata,axis=1))),type=2,axis=1)
+        LDATA = dct(np.column_stack((ldata,np.flip(ldata,axis=1))),type=2,axis=1)
+        np.savetxt('%s.rspect'%fname,np.abs(RDATA[:,:nsamples*2:2]).T)
+        np.savetxt('%s.lspect'%fname,np.abs(LDATA[:,:nsamples*2:2]).T)
+    else:
+        sdata = data[:sz].reshape((nfolds,nsamples)).T
+        #sdata = data[:sz].reshape((nsamples,nfolds))
+        SDATA = dct(np.row_stack((sdata,np.flip(sdata,axis=0))),type=2,axis=0)
+        np.savetxt('%s.sspect'%(fname),np.abs(SDATA[::2,:]),fmt='%.3f')
     print(samplerate)
 
     return
