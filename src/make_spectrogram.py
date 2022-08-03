e11eb991e22683fbff089fc64e0393deecde4836
Ryan Coffee <coffeer76@gmail.com>
2022-08-03 12:25:58 -0700

    updated for srcf signals

--- src/make_spectrogram.py
+++ src/make_spectrogram.py
@@ -7,23 +7,38 @@ import sys
 import wave
 
 def main():
+    if len(sys.argv)<3:
+        print("syntax:./src/make_spectrogram.py <path/file.WAV> <'bee'|'server'>")
     fname = sys.argv[1]
-    nsamples = 1<<14
-    nfolds = 1<<12
+    nsamples = 1<<12
+    nfolds = 1<<14
+    scale = 1<<12
+    flim = 12
+        if sys.argv[2]=='bee':
+            nsamples = 1<<14
+            nfolds = 1<<12
+            scale = 1<<10
+        elif sys.argv[2]=='server':
+            nsamples = 1<<12
+            nfolds = 1<<14
+            scale = 1<<12
+
+            
     dt = np.dtype(np.int16).newbyteorder('<')
     data = []
     with wave.open(fname,'r') as f:
         print(f.getparams())
         if f.getnchannels()>1:
             print('Ahhh, processing more than one channel!  Returning!!')
+            f.close()
             return
         samplewidth = f.getsampwidth()
         totframes = f.getnframes()
         while f.tell()<min(totframes - nsamples*samplewidth,nsamples*nfolds*samplewidth):
             tmp = np.frombuffer(f.readframes(nsamples*samplewidth),dtype=dt)
-            data += [np.abs(dct(np.concatenate((tmp,np.flip(tmp,axis=0))),type=2,axis=0)[:1<<12:2])//(1<<10)]
-            #data += [np.abs(dct(np.concatenate((tmp,np.flip(tmp,axis=0))),type=2,axis=0)[::2])//(1<<10)]
-    np.savetxt('%s.sspect'%(fname),np.array(data).T,fmt='%i',header='remember, highest frequency is 1/4 of the 96kHz (only showing 2**12 of 2**14 samples)')
+            data += [np.abs(dct(np.concatenate((tmp,np.flip(tmp,axis=0))),type=2,axis=0)[:1<<flim:2])//scale]
+            #data += [np.abs(dct(np.concatenate((tmp,np.flip(tmp,axis=0))),type=2,axis=0)[::2])//scale]
+    np.savetxt('%s.sspect'%(fname),np.array(data).T,fmt='%i',header='remember for %s, highest frequency is %i\% of the 96kHz (only showing 2**%i of 2**%i samples)'%(sys.argv[2],int(100*float(flim)/float(nsamples)),flim,nsamples) )
     return
 
     '''
